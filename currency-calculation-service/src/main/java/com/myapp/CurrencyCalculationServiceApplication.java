508dd77304f55763ad6b38dc7d0e5be6767f64f2
subhashissarangi <sarangi.subhashis@gmail.com>
2018-11-14 19:21:44 +0530

    first commit

---
+++ currency-calculation-service/src/main/java/com/myapp/CurrencyCalculationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCalculationServiceApplication.class, args);
+	}
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into employee(id,name)
+values(101,'a');
+insert into employee(id,name)
+values(102,'b');
