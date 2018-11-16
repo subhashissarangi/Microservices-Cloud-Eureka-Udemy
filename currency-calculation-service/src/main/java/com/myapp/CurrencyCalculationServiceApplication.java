dc944171b9805a3cf15526a9cb2e5c29c74ab33f
subhashissarangi <sarangi.subhashis@gmail.com>
2018-11-16 20:18:59 +0530

    Eureka server created

--- currency-calculation-service/src/main/java/com/myapp/CurrencyCalculationServiceApplication.java
+++ currency-calculation-service/src/main/java/com/myapp/CurrencyCalculationServiceApplication.java
@@ -2,8 +2,10 @@ package com.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.myapp")
 public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
--- currency-calculation-service/src/main/java/com/myapp/controller/CurrencyConverterController.java
+++ currency-calculation-service/src/main/java/com/myapp/controller/CurrencyConverterController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +17,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.myapp.bean.CurrencyConvertionBean;
 import com.myapp.exceptions.UserDefindException;
+import com.myapp.services.proxy.CurrencyExchangeServiceProxy;
 
 @RestController
-
 public class CurrencyConverterController {
 
-	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/1000")
-	@ExceptionHandler(UserDefindException.class)
-	@ResponseStatus
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	// @ExceptionHandler(UserDefindException.class)
+	// @ResponseStatus
 	public CurrencyConvertionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 		Map<String, String> uriVariables = new HashMap<>();
@@ -32,6 +37,21 @@ public class CurrencyConverterController {
 				"http://localhost:8000/currecny-exchange/from/{from}/to/{to}", CurrencyConvertionBean.class,
 				uriVariables);
 		CurrencyConvertionBean response = responseEntity.getBody();
+		return new CurrencyConvertionBean(response.getId(), from, to, response.getConversionMultiple(),
+				response.getQuantity(), quantity.multiply(response.getTotalCalculatedAmount()), response.getPort());
+	}
+
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConvertionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		/*
+		 * Map<String, String> uriVariables = new HashMap<>(); uriVariables.put("from",
+		 * from); uriVariables.put("to", to); ResponseEntity<CurrencyConvertionBean>
+		 * responseEntity = new RestTemplate().getForEntity(
+		 * "http://localhost:8000/currecny-exchange/from/{from}/to/{to}",
+		 * CurrencyConvertionBean.class, uriVariables);
+		 */
+		CurrencyConvertionBean response = proxy.retriveExchangeValue(from, to);
 		return new CurrencyConvertionBean(response.getId(), from, to, response.getConversionMultiple(),
 				response.getQuantity(), quantity.multiply(response.getTotalCalculatedAmount()), response.getPort());
 	}
