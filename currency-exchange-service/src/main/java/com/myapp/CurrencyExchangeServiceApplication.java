b19ad3cc815c14116baa657d60e7812daf7db300
subhashissarangi <sarangi.subhashis@gmail.com>
2018-11-21 16:37:52 +0530

    eureka server established

--- currency-exchange-service/src/main/java/com/myapp/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/myapp/CurrencyExchangeServiceApplication.java
@@ -2,10 +2,11 @@ package com.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = { "com" })
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
